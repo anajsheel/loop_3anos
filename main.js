01ee2e3fd227b8d78622d2cca8aa848b585e32a4
ana julia santos <106815236+anajsheel@users.noreply.github.com>
2024-04-16 12:15:12 -0300

    mid

--- main.js
+++ main.js
@@ -6,13 +6,9 @@ function verificarTime() {
   while (continuarPerguntando) {
     
     var respostaTime = prompt("Quantas mundiais o palmeiras tem?");
-    var respostaTime = prompt("Santos está na serie A?")
-    var respostaTime = prompt("Em qual ano São Paulo tomou 6x1 contra o corinthians?")
-
+   
    if (respostaTime.toLowerCase() === "Nenhuma") {
-    if (respostaTime.toLowerCase() === "Série B") {
-      if (respostaTime.toLowerCase() === "2015"){
-        if (respostaTimet.toLowerCase() === "Neo quimica arena"){
+    
       alert("Isso mesmo! Continue assim.");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
