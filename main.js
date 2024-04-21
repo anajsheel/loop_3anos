1305141a3aa142208e2c025a2f4c1af210c2cab8
ana julia santos <106815236+anajsheel@users.noreply.github.com>
2024-04-20 23:12:03 -0300

    mid

--- main.js
+++ main.js
@@ -21,3 +21,23 @@ function verificarTime() {
 verificarTime();
 
   
+function verificarTime() {
+  
+  var continuarPerguntando = true;
+
+  while (continuarPerguntando) {
+    
+    var respostaTime = prompt("O Santos está na série A ou B?");
+   
+   if (respostaTime.toLowerCase() === "Série B") {
+    
+      alert("Isso mesmo! Continue assim.");
+      continuarPerguntando = false; // Sai do loop após a resposta correta
+    } else {
+      alert("Incorreto! Tente novamente.");
+    }
+  }
+}
+
+// Chamada da função para iniciar o processo
+verificarTime();
